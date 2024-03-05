4da5c56c04e9e17f24d0667befd1be96d68fde19
amitbisht9 <124331439+amitbisht9@users.noreply.github.com>
2024-03-05 13:28:58 +0530

    fixed status parsing issue

--- test-report-parser.py
+++ test-report-parser.py
@@ -7,26 +7,27 @@ root = tree.getroot()
 
 # Define a list to store test results
 test_results = []
-
 # Iterate over test cases
+test_elements = root.findall('.//test')
 for test_case in root.findall('.//test'):
-    test_id = test_case.get('id')
+    #test_id = test_case.get('id')
+    test_id = test_case.find('tag').text
+    #print(test_id)
     test_name = test_case.get('name')
-    test_duration_str = test_case.find('.//status').get('elapsed')
+    test_duration_str = test_case.find('./status').get('elapsed')
     test_duration = float(test_duration_str)
-    test_status = test_case.find('.//status').get('status')
-
+    test_status = test_case.find('./status').get('status')
     # Convert elapsed time to milliseconds without decimal points
     test_duration = int(float(test_duration_str) * 1000)
-
+      
     # Set test_status to "Passed" if status is "PASS", otherwise set it to "Failed"
-    if test_status == "PASS":
+    if test_status.upper() == "PASS":
         test_status = "Passed"
-    elif test_status == "FAIL":
+    elif test_status.upper() == "FAIL":
         test_status = "Failed"
     else:
         test_status = "UnKnown"
-
+    # print(test_status)
     # Create a dictionary for each test result and append it to the list
     test_result = {
         "test_id": test_id,
